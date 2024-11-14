f8de9ce35a47bc06a6a84d22e72fe1aa45ed4da0
TIM RICE <tdrice668@gmail.com>
2024-11-13 22:22:17 -0600

    trying to fix deploy errors

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,17 +1,6 @@
-// const Nav = () => {
-//   // TODO: Add necessary code to display the navigation bar and link between the pages
-//   return (
-//     <div>Nav</div>
-
-//   )
-// };
-
-// export default Nav;
-
-// import React from 'react';
 import { Link } from "react-router-dom";
 
-const Nav = () => {
+const Nav: FC = () => {
   return (
     <nav>
       <ul style={{ display: "flex", gap: "1rem", listStyle: "none" }}>
